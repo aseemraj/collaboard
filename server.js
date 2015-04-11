24967ca2f5cdbbcf14b518dbc03656b4d06bcf81
aseemraj <ug201210005@iitj.ac.in>
2015-04-12 00:49:09 +0530

    went offline feature

--- server.js
+++ server.js
@@ -24,7 +24,6 @@ var entityMap = {
     "/": '&#x2F;'
 };
 
-var sockids = new Object();
 var socknames = new Object();
 
 function escapeHtml(string) {
@@ -37,31 +36,25 @@ var io = require('socket.io').listen(app.listen(port));
 
 io.sockets.on('connection', function (socket) {
     socket.emit('idreport', { sockid: socket.id });
+    socket.emit('idreport', { sockid: socket.id });
     socket.on('joinserver', function(data) {
-        delete socknames[sockids[escapeHtml(data.username)]];
-        sockids[escapeHtml(data.username)] = socket.id;
         socknames[socket.id] = escapeHtml(data.username);
-        io.sockets.emit("message", { username: 'Server', message: escapeHtml(data.username) + ' just joined!' });
+        socket.broadcast.emit("message", { username: 'Server', message: escapeHtml(data.username) + ' appeared online!' });
     });
     socket.on('send', function (data) {
-        // io.sockets.emit('message', data);
         io.sockets.emit('message', { message: escapeHtml(data.message), username: escapeHtml(data.username) });
     });
     socket.on('namechange', function (data) {
-        sockids[escapeHtml(data.newname)] = socket.id;
         socknames[socket.id] = escapeHtml(data.newname);
-        delete sockids[escapeHtml(data.oldname)];
-        io.sockets.emit('message', { username: 'Server', message: '<b>'+data.oldname+'</b> changed their name to <b>'+data.newname+'</b>' });
+        socket.broadcast.emit('message', { username: 'Server', message: '<b>'+data.oldname+'</b> changed their name to <b>'+data.newname+'</b>' });
     });
     socket.on('drawn', function (data) {
         io.sockets.emit('draw', data);
     });
     socket.on("disconnect", function (data) {
         var name = socknames[socket.id];
-        var sockid = socket.id;
-        io.sockets.emit("message", { username: 'Server', message: name + ' went offline!' });
-        delete sockids[name];
-        delete socknames[sockid];
+        delete socknames[socket.id];
+        io.sockets.emit("message", { username: 'Server', message: '<b>' + name + '</b> went offline!' });
     });
 });
 
